2346f42a9974c35f87b0b54072187e078fad87bf
MyProjectGroup <67843045+niebza12@users.noreply.github.com>
2020-12-20 22:05:32 +0700

    Programe Test V.1

---
+++ Test/Program/Program/MainMenu.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Program
+{
+    /// <summary>
+    /// Interaction logic for MainMenu.xaml
+    /// </summary>
+    public partial class MainMenu : Window
+    {
+        public MainMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void StackPanel_ColorChanged(object sender, RoutedPropertyChangedEventArgs<Color> e)
+        {
+
+        }
+
+        private void Chip_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
