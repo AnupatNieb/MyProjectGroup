b0e80a128627e3e797489704916bcc8334d8822b
MyProjectGroup <67843045+niebza12@users.noreply.github.com>
2021-01-04 15:37:38 +0700

    ทำเมนูเพิ่มเติม, ฐานข้อมูลใหม่

---
+++ Project กะเพราถาด/Program/Program V.2/Program/Dashboard/Dashboard.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Program
+{
+    /// <summary>
+    /// Interaction logic for Dashboard.xaml
+    /// </summary>
+    public partial class Dashboard : Window
+    {
+        public Dashboard()
+        {
+            InitializeComponent();
+            SubPanel.Content = new DashboardPage();
+        }
+
+        private void Window_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Escape)
+            {
+                this.Close();
+            }
+        }
+
+        private void exit_btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void menu_btn_Click(object sender, RoutedEventArgs e)
+        {
+            SubPanel.Content = new MenuPage();
+        }
+
+        private void dashboard_btn_Click(object sender, RoutedEventArgs e)
+        {
+            SubPanel.Content = new DashboardPage();
+        }
+    }
+}
